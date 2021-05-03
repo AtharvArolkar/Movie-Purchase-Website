4bf8966d984e3cdd2142a2347c4c3d2a34d07574
Gaurang Govekar <gaurangsg.28@gmail.com>
2021-05-03 22:21:05 +0530

    make slider based on number of images

--- slider.js
+++ slider.js
@@ -1,32 +1,47 @@
 var counter = 1;
 var counter_auto=1;
+var images = document.getElementsByClassName('slide')
 
-    setInterval(function () {
-        if(counter_auto==counter){
-          counter++;
-          counter_auto++;
-        }else{
-          counter_auto=counter;
-        }
-        if (counter_auto > 4) {
-            counter = 1;
-            counter_auto=1;
-        }
-        document.getElementById('radio' + counter).checked = true;
+ $(document).ready(
+   function(){
+     var i=0;
+     for (let image of images){
+       i++;
+       $(".slide.first").before('<input type="radio" name="radio-btn" id="radio'+i+'" value="'+i+'">');
+       $(".slider-nav").append('<label for="radio'+i+'" class="slider-nav-btn'+i+'"></label>');
+       $('<style>#radio'+i+':checked ~ .first{margin-left:'+(i-1)*(-10)+'%;</style>}').appendTo("head");
+       $('<style>#radio'+i+':checked ~ .slider-nav .slider-nav-btn'+i+'{background:#ffffff;</style>}').appendTo("head");
 
-    }, 5000);
-
-    function prev(){
-        counter--;
-        if (counter == 0) {
-            counter = 4;
-        }
-        document.getElementById('radio' + counter).checked = true;
+     }
+   }
+ )
+setInterval(function () {
+    console.log(counter);
+    if(counter_auto==counter){
+      counter++;
+      counter_auto++;
+    }else{
+      counter_auto=counter;
     }
-    function next(){
-        counter++;
-        if (counter == 5) {
-            counter = 1;
-        }
-        document.getElementById('radio' + counter).checked = true;
+    if (counter_auto > images.length) {
+        counter = 1;
+        counter_auto = 1;
     }
+    document.getElementById('radio' + counter).checked = true;
+}, 5000);
+
+function prev(){
+    counter--;
+    if (counter == 0) {
+        counter = images.length;
+    }
+    document.getElementById('radio' + counter).checked = true;
+}
+function next(){
+    counter++;
+    if (counter > images.length) {
+        counter = 1;
+    }
+    document.getElementById('radio' + counter).checked = true;
+}
+''
